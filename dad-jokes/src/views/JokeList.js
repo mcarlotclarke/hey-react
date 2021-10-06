c5f515daf61689ceabd1037c0119fcbbfc09bd62
MCClarke <mcarlotclarke@gmail.com>
2021-10-05 21:09:58 -0400

    App and JokeList styles

--- dad-jokes/src/views/JokeList.js
+++ dad-jokes/src/views/JokeList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Joke from './Joke';
+import '../styles/JokeList.css';
 
 const API_URL = 'https://icanhazdadjoke.com/';
 
@@ -32,7 +33,16 @@ class JokeList extends Component {
   render() {
     return (
       <div className="JokeList">
-        <h1>Dad's Jokes!</h1>
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            <span>Dad</span> Jokes!
+          </h1>
+          <img
+            src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+            alt="Icon"
+          />
+          <button className="JokeList-getmore">New Jokes</button>
+        </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map((joke) => (
             <ul>
